540b4dd5afbc212d34ef7c6f92490419e28cbbd3
uni7mau <hoangnamunicorn2208@gmail.com>
2025-09-23 21:15:45 +0700

    First commit

---
+++ src/main/java/com/demos/demostealprocessserver/DemoStealProcessServerApplication.java
@@ -0,0 +1,13 @@
+package com.demos.demostealprocessserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStealProcessServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoStealProcessServerApplication.class, args);
+    }
+
+}
